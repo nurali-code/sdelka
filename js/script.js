68392bf82170ef427dc75c8d9cf28ac9772381aa
nurali-code <nur3.dav.97@gmail.com>
2024-01-30 18:07:23 +0500

    modal done

--- js/script.js
+++ js/script.js
@@ -7,6 +7,13 @@ document.querySelectorAll('.services-item').forEach(function (item) {
     });
 });
 
+document.querySelectorAll('.modal_toggle').forEach(function (modalOpen) {
+    modalOpen.addEventListener('click', function () {
+        document.getElementById('modal').classList.toggle('is_active');
+    });
+});
+
+
 // Для .capability .js
 document.querySelectorAll('.capability .js').forEach(function (item) {
     item.addEventListener('mouseenter', function () {
@@ -14,14 +21,14 @@ document.querySelectorAll('.capability .js').forEach(function (item) {
         Array.from(imgs).forEach(function (img) {
             img.style.display = 'none';
         });
-        imgs[0].style.display = 'block';
-        var currentImg = 0;
+        imgs[1].style.display = 'block'; // Показываем вторую картинку
+        var currentImg = 1; // Устанавливаем индекс второй картинки
         function fadeInNextImage() {
             imgs[currentImg].style.display = 'none';
             currentImg = (currentImg + 1) % imgs.length;
             imgs[currentImg].style.display = 'block';
         }
-        var interval = setInterval(fadeInNextImage, 800);
+        var interval = setInterval(fadeInNextImage, 500);
         this.setAttribute('data-interval', interval);
     });
 
@@ -29,11 +36,11 @@ document.querySelectorAll('.capability .js').forEach(function (item) {
         clearInterval(this.getAttribute('data-interval'));
         var imgs = this.querySelectorAll('.item-img img');
         Array.from(imgs).forEach(function (img, index) {
-            if (index !== 0) {
+            if (index !== 1) { // Скрываем все картинки, кроме второй
                 img.style.display = 'none';
             }
         });
-        imgs[0].style.display = 'block';
+        imgs[1].style.display = 'block'; // Показываем вторую картинку
     });
 });
 
